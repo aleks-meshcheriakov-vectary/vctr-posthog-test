d3e3d01fc78dae28791c06f342228c1f11879f22
Aleksandr Meshcheriakov @ Vectary <meshcheriakov@vectary.com>
2024-09-17 12:05:32 +0200

    add autocapture

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 // @ts-ignore
-// import posthog from "https://esm.sh/posthog-js@1.161.5";
+// import _posthog from "posthog-js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 	// @ts-ignore
@@ -10,6 +10,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 	posthog.init("phc_gCjaeBt5lx578jmFbMjb0ltJt8012o7Dhl5udaR7YIF", {
 		api_host: "https://eu.i.posthog.com",
 		person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+		autocapture: {
+			dom_event_allowlist: ["click"], // DOM events from this list ['click', 'change', 'submit']
+			// url_allowlist: ['posthog.com./docs/.*'], // strings or RegExps
+			// url_ignorelist can be used on its own, or combined with url_allowlist to further filter which URLs are captured
+			// url_ignorelist: ['posthog.com./docs/.*/secret-section/.*'], // strings or RegExps
+			element_allowlist: ["a", "button", "form"], // DOM elements from this list ['a', 'button', 'form', 'input', 'select', 'textarea', 'label']
+			css_selector_allowlist: ["[ph-autocapture]"], // List of CSS selectors
+			// element_attribute_ignorelist:['data-attr-pii="email"'], // List of element attributes to ignore
+		},
 	});
 
 	posthog.onFeatureFlags(function () {
